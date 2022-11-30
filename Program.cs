ef40c9770a1d614f748eb5198f532a4e2ba2ac6c
Milena Elen <elenprojects7@gmail.com>
2022-11-30 10:49:07 -0300

    FEAT: create validations and peca function

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace xadrez_console
+﻿using tabuleiro;
+using tabuleiro.Enum;
+using xadrez;
+using xadrez_console.tabuleiro.Exceptions;
+
+namespace xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Tabuleiro tabuleiro = new(8, 8);
+
+                Peca peca = new(Cor.Preta, tabuleiro);
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 9));
+                tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+
+                //Console.WriteLine("Posicao: " + peca.Posicao);
+
+                Tela.ImprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
