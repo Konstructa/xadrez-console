cfb4cfabbe1c5e96a7fb472a6c3490fde0e7e05e
Milena Elen <elenprojects7@gmail.com>
2022-11-30 09:54:58 -0300

    FEAT: add function PECA

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -15,5 +15,10 @@ namespace tabuleiro
             Colunas = colunas;
             Pecas = new Peca[linhas, colunas];
         }
+
+        public Peca Peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
+        }
     }
 }
